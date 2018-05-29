395a6aee1ff1a584330674efd84ec92be05f619b
Cfunking-Nor <39250389+Cfunking-Nor@users.noreply.github.com>
2018-05-29 19:25:24 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<title>CRUD Operations</title>
+</head>
+<body>
+
+<form name="Log-In-Form" action="loginOps.php" method="POST">
+
+<input type="text" name="userName" placeholder="Enter your Username"/>
+<br>
+<input type="password" name="pWord" placeholder="Enter your Password"/>
+<br>
+<input type="submit" value="Log-In"/>
+</form>
+
+</body>
+</html>
